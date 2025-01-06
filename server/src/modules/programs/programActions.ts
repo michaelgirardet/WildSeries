3aa3abed958d3e8cc6010e16844ae845c845dbcb
michaelgirardet <michaelgirardet@outlook.com>
2025-01-06 11:46:46 +0100

    create Repository

--- server/src/modules/programs/programActions.ts
+++ server/src/modules/programs/programActions.ts
@@ -1,3 +1,6 @@
+// Import access to data
+import programRepository from "./programRepository";
+
 // Some data to make the trick
 
 const programs = [
@@ -27,16 +30,10 @@ const programs = [
 
 import type { RequestHandler } from "express";
 
-const browse: RequestHandler = (req, res) => {
-  if (req.query.q != null) {
-    const filteredPrograms = programs.filter((program) =>
-      program.synopsis.includes(req.query.q as string),
-    );
+const browse: RequestHandler = async (req, res) => {
+  const programsFromDB = await programRepository.readAll();
 
-    res.json(filteredPrograms);
-  } else {
-    res.json(programs);
-  }
+  res.json(programsFromDB);
 };
 
 const read: RequestHandler = (req, res) => {
